1b66359e2a0cdc94ac7a4d644c93ad21928e63e5
Akrivia-Intern <learnerdaily5@gmail.com>
2025-01-01 12:29:15 +0530

    Week 1

---
+++ Week 1/Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) // Lazy load DashboardModule
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule) // Lazy load LoginModule
+  },
+  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule) // Lazy load RegistrationModule
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { enableTracing: false })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
